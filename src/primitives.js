2618883c81d537765edff43583b05da44fa86684
Seth Kinast <skinast@linkedin.com>
2016-05-12 23:07:19 -0700

    No need to short-circuit common primitives because webpack will automatically inline them

--- src/primitives.js
+++ src/primitives.js
@@ -1,12 +1,9 @@
 module.exports = {
-  fixed: 'Model.STRING',
   long: 'Model.INT',
-  string: 'Model.STRING',
   int: 'Model.INT',
+  fixed: 'Model.STRING',
+  string: 'Model.STRING',
   double: 'Model.FLOAT',
   float: 'Model.FLOAT',
-  boolean: 'Model.BOOL',
-  'com.linkedin.common.time': 'Model.DATE',
-  'com.linkedin.common.Urn': 'Model.STRING',
-  'com.linkedin.voyager.common.Urn': 'Model.STRING'
+  boolean: 'Model.BOOL'
 };
